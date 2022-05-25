4e2f07efe658d69c66c337251881d62dfaa3f8ee
Rose <rosemwangi305@gmail.com>
2022-05-25 08:40:14 +0300

    minor updates

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react'
+import Category from './components/Category'
 import Pages from './pages/Pages'
+import {BrowserRouter} from 'react-router-dom'
 
 
 const App = () => {
   return (
     <div className="app">
+      <BrowserRouter>
+      <Category />
       <Pages />
+      </BrowserRouter>
     </div>
   )
 }
