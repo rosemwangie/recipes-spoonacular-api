2462e8b198c296c8e34748bacf3c1ee5bdcac21e
Rose <rosemwangi305@gmail.com>
2022-05-28 16:21:50 +0300

    loading pages changes

--- src/pages/Pages.jsx
+++ src/pages/Pages.jsx
@@ -1,21 +1,23 @@
 import React from 'react'
 import Home from './Home'
-import {Route,Routes} from 'react-router-dom'
+import {Route,Routes, useLocation} from 'react-router-dom'
 import Cuisine from './Cuisine'
 import Searched from './Searched'
+import Recipe from './Recipe'
+import { AnimatePresence } from 'framer-motion'
 
 const Pages = () => {
+  const location = useLocation()
   return (
     <div className="pages">
-
-      <Routes>
+      <AnimatePresence exitBeforeEnter>
+      <Routes location={location} key={location.pathname}>
         <Route path='/' element={<Home />}/>
         <Route path='/cuisine/:type' element={<Cuisine />}/>
-        <Route path="/search/:search" element={<Searched/>}/>
-      </Routes>
-  
-
-      
+        <Route path='/searched/:search' element={<Searched/>}/>
+        <Route path='/recipe/:name' element={<Recipe/>}/>
+      </Routes>  
+      </AnimatePresence>         
     </div>
   )
 }
